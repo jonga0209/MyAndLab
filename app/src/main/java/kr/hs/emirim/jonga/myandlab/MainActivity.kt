de22fba63ab614949f23078ab3614ded6d725dbf
Jonga <whddk0209@gmail.com>
2020-10-27 20:09:21 +0900

    #2심플계산기

--- app/src/main/java/kr/hs/emirim/jonga/myandlab/MainActivity.kt
+++ app/src/main/java/kr/hs/emirim/jonga/myandlab/MainActivity.kt
@@ -5,23 +5,23 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.ImageView
+import kr.hs.emirim.jonga.myandlab.ch04widget.CalActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     lateinit var mButton1 : Button
     lateinit var mButton2 : Button
-    lateinit var mProfile : ImageView
     lateinit var mButton3 : Button
+    lateinit var mButton4 : Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        mProfile = findViewById(R.id.profile)
         mButton1 = findViewById(R.id.button1)
         mButton2 = findViewById(R.id.button2)
         mButton3 = findViewById(R.id.button3)
+        mButton4 = findViewById(R.id.button4)
 
         mButton1.setOnClickListener {
             startActivity(Intent(this, FourbuttonActivity::class.java))
@@ -31,6 +31,9 @@ class MainActivity : AppCompatActivity() {
         }
         mButton3.setOnClickListener {
             startActivity(Intent(this, Homework1Activity::class.java))
+        }
+        mButton4.setOnClickListener {
+            startActivity(Intent(this, CalActivity::class.java))
         }
     }
 }
---
+++ app/src/main/java/kr/hs/emirim/jonga/myandlab/ch04widget/CalActivity.kt
@@ -0,0 +1,12 @@
+package kr.hs.emirim.jonga.myandlab.ch04widget
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kr.hs.emirim.jonga.myandlab.R
+
+class CalActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_cal)
+    }
+}
